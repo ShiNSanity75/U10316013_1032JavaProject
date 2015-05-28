1f7ae99c1e11fb06d92add0d7184dbd7d1b885d5
ShiNSanity75 <jj06280705@gmail.com>
2015-05-28 23:36:45 +0800

    可產生不同的圖片(目前只放一張)

--- GuessGame.java
+++ GuessGame.java
@@ -10,6 +10,11 @@ public class GuessGame extends JFrame {
 	//Create a label to put picture
 	JLabel jlbl = new JLabel();
 	
+	//Create an array to store the random list
+	int num[] = new int[100];
+	boolean isSame;
+	int i;
+	
 	
 	public GuessGame() {
 		
@@ -18,10 +23,26 @@ public class GuessGame extends JFrame {
 			picture[k] = new ImageIcon("icon/"+(k+1)+".jpg");
 		} 
 		
+		//Create the random list
+		for(i = 0; i < 100; i++) { 
+			isSame = true;
+			
+			while(isSame){
+				isSame = false;
+				num[i] = (int)(Math.random() * 100);
+
+				for(int j = 0; j < i; j++) {
+					if(num[i] == num[j]) {
+						isSame = true;
+					}
+				}
+				
+			}
+		}
+		
 		//Create panel p1 to display the picture
 		JPanel p1 = new JPanel();
-		int i = 40;
-		jlbl = new JLabel(picture[i]);
+		jlbl = new JLabel(picture[num[30]]);
 		jlbl.setHorizontalTextPosition(JLabel.CENTER);
 		
 		p1.add(jlbl);
@@ -34,7 +55,7 @@ public class GuessGame extends JFrame {
 	public static void main (String[] args) {
 		GuessGame frame = new GuessGame(); //Create a frame
 		frame.setTitle("名人領域猜一猜"); //set the title of frame
-		frame.setSize(400, 500); //Set the size of frame
+		frame.setSize(500, 600); //Set the size of frame
 		frame.setLocationRelativeTo(null); //Center the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true); //Display the frame
