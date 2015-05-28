fc29fc471736ae45528bad1522ceb1019f92668a
ShiNSanity75 <jj06280705@gmail.com>
2015-05-28 21:05:27 +0800

    試放照片(只是放一張看位置)

--- GuessGame.java
+++ GuessGame.java
@@ -4,6 +4,31 @@ import javax.swing.*;
 
 public class GuessGame extends JFrame {
 
+	//Create an array to store the 100 picture
+	ImageIcon[] picture = new ImageIcon[100];
+	
+	//Create a label to put picture
+	JLabel jlbl = new JLabel();
+	
+	
+	public GuessGame() {
+		
+		//Create an image icon from an icon file
+		for(int k = 0; k < 100; k++) {
+			picture[k] = new ImageIcon("icon/"+(k+1)+".jpg");
+		} 
+		
+		//Create panel p1 to display the picture
+		JPanel p1 = new JPanel();
+		int i = 40;
+		jlbl = new JLabel(picture[i]);
+		jlbl.setHorizontalTextPosition(JLabel.CENTER);
+		
+		p1.add(jlbl);
+			
+	
+		add(p1);
+	}
 
 	/**Main Method*/
 	public static void main (String[] args) {
